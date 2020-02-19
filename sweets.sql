5ae8b48590023b9b8291037dd5756ce5b28cdb5f
Zizipho <61008677+lituka@users.noreply.github.com>
2020-02-19 12:06:15 +0200

    Add files via upload

---
+++ sweets.sql
@@ -0,0 +1,5 @@
+type,price,quantity,size,barcode,id,description,stockprice
+amajoya,R25,25,500g,34569872,1,mint,R10
+cardbury,R35,20,500g,25498726,2,choc,R15
+jellytots,R15,30,900g,65894213,3,original,R8
+halls,R10,20,300g,15478962,4,airwave,R5
