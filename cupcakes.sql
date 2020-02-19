dcab54314bd2ad4fbe95bf69b6820469a9fde05e
Zizipho <61008677+lituka@users.noreply.github.com>
2020-02-19 12:04:36 +0200

    Add files via upload

---
+++ cupcakes.sql
@@ -0,0 +1,5 @@
+type,price,quantity,size,barcode,id,description,stockprice
+vannilla,R20,20,50g,65849213,1,vanilla,R10
+chocmint,R17,20,50g,48521673,2,mint,R10
+browny,R22,15,50g,32564871,3,choc,R15
+chocolate,R20,10,50g,65842931,4,choc,R10
