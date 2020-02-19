c6b8bbc2f68899694229528f2a59484fd15fdcb3
Zizipho <61008677+lituka@users.noreply.github.com>
2020-02-19 12:03:19 +0200

    Add files via upload

---
+++ veggies.sql
@@ -0,0 +1,5 @@
+type,price,quantity,size,barcode,id,description,stockprice
+spinach,15,10,500g,54896325,1,original,R8
+onion,20,15,1kg,84759612,2,original,R10
+butternut,10,20,400g,95862347,3,original,R5
+potatoes,50,10,5kg,35462189,4,original,R30
