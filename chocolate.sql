d390671bd8134474ab5c37273eb9234fab7988eb
Zizipho <61008677+lituka@users.noreply.github.com>
2020-02-19 12:03:42 +0200

    Add files via upload

---
+++ chocolate.sql
@@ -0,0 +1,5 @@
+type,price,quantity,size,barcode,id,description,stockprice
+ps,R8,20,75g,35489627,1,milky,R5
+kitkat,R5,15,40g,95876421,2,mint,R3
+barone,R13,10,75g,45876215,3,choc,R8
+tex,R7,25,49g,68549278,4,mint,R4
