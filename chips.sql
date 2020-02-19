4463befbf553b80d7473e5ebda483349d33d079e
Zizipho <61008677+lituka@users.noreply.github.com>
2020-02-19 11:34:21 +0200

    Add files via upload

---
+++ chips.sql
@@ -0,0 +1,5 @@
+type,price,quantity,size,barcode,id,description,stockprice
+doritos,R15,20,500g,68541279,1,chilli,R12
+lays,R12,15,250g,89544786,2,tomato,R8
+niknaks,R5,30,59g,45789625,3,cheese,R2
+simba,R18,20,500g,68945726,4,shisanyama,R10
