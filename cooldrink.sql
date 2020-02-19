2d5d6c62efaffd8b1361f144d611e2b695a56ebb
Zizipho <61008677+lituka@users.noreply.github.com>
2020-02-19 12:04:06 +0200

    Add files via upload

---
+++ cooldrink.sql
@@ -0,0 +1,5 @@
+type,price,quantity,size,barcode,id,description,stockprice
+sprite,R14,10,500ml,54867219,1,original,R9
+fanta,R11,10,500ml,62145892,2,orange,R7
+jive,R10,20,2l,96854213,3,pineapple,R5
+score,R10,15,500ml,26531489,4,original,R5
