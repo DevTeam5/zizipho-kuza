e7c427d91048026ee8d2619d3ddcfd120528d143
Zizipho <61008677+lituka@users.noreply.github.com>
2020-02-19 12:05:44 +0200

    Add files via upload

---
+++ meat.sql
@@ -0,0 +1,5 @@
+type,price,quantity,size,barcode,id,description,stockprice
+chicken,50,10,2kg,62548913,1,thighs,R25
+pork,35,20,1kg,35684129,2,bones,R15
+sausage,20,25,1kg,64879521,3,sausage,R10
+mutton,60,10,1kg,34587962,4,tongues,R30
