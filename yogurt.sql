efeaa647e6af239a8813ce2e8122dbdbec46825b
Zizipho <61008677+lituka@users.noreply.github.com>
2020-02-19 12:07:00 +0200

    Add files via upload

---
+++ yogurt.sql
@@ -0,0 +1,5 @@
+type,price,quantity,size,barcode,id,description,stockprice
+danone,R20,20,1kg,25489617,1,vanilla,R10
+parmalat,R15,20,500g,45812364,2,peach,R8
+clover,R10,15,500g,48759621,3,choc,R5
+sonnedal,R13,30,1kg,13587469,4,berry,R7
