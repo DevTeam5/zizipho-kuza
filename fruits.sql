72da6e9cb41179916847ff67e863bb38055e546a
Zizipho <61008677+lituka@users.noreply.github.com>
2020-02-19 12:05:05 +0200

    Add files via upload

---
+++ fruits.sql
@@ -0,0 +1,5 @@
+type,price,quantity,size,barcode,id,description,stockprice
+guava,R10,20,1kg,15248637,1,guava,R5
+peach,R12,15,1kg,54862374,2,peach,R7
+pear,R8,30,1kg,85469721,3,pear,R4
+banana,R15,10,1kg,95874216,4,banana,R10
